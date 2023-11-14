98c0687201394863643c641c6f1c3df7d4aae2ec
Olya Kutskova <kutskova.olga95@mail.ru>
2023-11-14 11:51:39 +0200

    styles replace

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -47,5 +47,40 @@ const store = useSearchStore();
   align-items: center;
   gap: 10px;
   justify-content: center;
+  
+  .cards {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    max-height: 80%;
+    overflow-y: scroll;
+
+    // для скроллбара
+    padding-right: 20px;
+    margin-right: -20px;
+  }
+
+  .nothing {
+    font-size: 18px;
+    color: var(--primary-text);
+    opacity: 0.5;
+    margin-top: 20px;
+    height: 50px;
+  }
+
+  .loader {
+    border: 5px solid white;
+    border-top: 5px solid var(--primary);
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    margin-top: 20px;
+    animation: spin 2s linear infinite;
+
+    @keyframes spin {
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+    }
+  }
 }
 </style>
